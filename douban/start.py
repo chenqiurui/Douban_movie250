928652cd3b0f5a920376a1850445fd3c985ed7b1
cqr <13387584220@163.com>
2019-03-06 09:29:13 +0800

    Update start.py

--- douban/start.py
+++ douban/start.py
@@ -1,1 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from scrapy import cmdline
+
+# 开始爬取
+#cmdline.execute('scrapy crawl douban_spider'.split())
+# 存储数据
+cmdline.execute('scrapy crawl douban_spider -o douban_movie.csv -t csv'.split())
+# 导出数据
+#cmdline.execute('sz douban_movie.csv'.split())
 
